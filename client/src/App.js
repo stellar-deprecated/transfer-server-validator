e9b44c1d48f68dcd01ecfc130b3c5cdba1a7b93e
Michael Feldstein <msfeldstein@gmail.com>
2020-02-04 15:06:41 -0800

    Add an error message

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,25 @@ import "./App.css";
 import DomainInput from "./DomainInput";
 import Loading from "./Loading";
 import ResultSet from "./ResultSet";
+import ErrorMessage from "./ErrorMessage";
 
 function App() {
   const [results, setResults] = useState(null);
   const [isLoading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
   const runValidation = useCallback(async domain => {
     setLoading(true);
     setResults(null);
-    const response = await fetch("/run?domain=" + domain);
-    //todo error handle
-    const json = await response.json();
-    setResults(json);
+    setErrorMessage(null);
+    try {
+      const response = await fetch("/run?domain=" + domain);
+      //todo error handle
+      const json = await response.json();
+      setResults(json);
+    } catch (e) {
+      console.log(e);
+      setErrorMessage("Something went wrong");
+    }
     setLoading(false);
   }, []);
 
@@ -23,6 +31,7 @@ function App() {
       <DomainInput onValidateClick={runValidation} isLoading={isLoading} />
       <Loading active={isLoading} />
       <ResultSet results={results} />
+      <ErrorMessage message={errorMessage} />
     </div>
   );
 }
