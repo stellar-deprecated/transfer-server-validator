554f4edf7b4276144f69a77dd5028719081002ee
Michael Feldstein <msfeldstein@gmail.com>
2020-02-04 17:11:52 -0800

    Fix merge

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,43 @@ import ResultSet from "./ResultSet";
 function App() {
   const [results, setResults] = useState(null);
   const [isLoading, setLoading] = useState(false);
-  const runValidation = useCallback(async domain => {
-    setLoading(true);
-    setResults(null);
-    const response = await fetch("/run?domain=" + domain);
-    //todo error handle
-    const json = await response.json();
-    setResults(json);
-    setLoading(false);
-  }, []);
+  const [loadingMessage, setLoadingMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const runValidation = useCallback(
+    async domain => {
+      setLoading(true);
+      setResults(null);
+      setErrorMessage(null);
+      try {
+        const evtSource = new EventSource(`/run?domain=${domain}`);
+        evtSource.addEventListener("message", event => {
+          console.log("Message", event.data);
+          const message = JSON.parse(event.data);
+          if (message.loadingMessage) {
+            setLoadingMessage(message.loadingMessage);
+          } else if (message.results) {
+            setResults(message.results);
+            setLoading(false);
+          }
+        });
+
+        evtSource.onerror = e => {
+          console.log(e);
+          setErrorMessage("Something went wrong");
+        };
+      } catch (e) {
+        console.log(e);
+        setErrorMessage("Something went wrong");
+        setLoading(false);
+      }
+    },
+    [setLoading, setLoadingMessage, setResults, setErrorMessage]
+  );
 
   return (
     <div className="App">
       <DomainInput onValidateClick={runValidation} isLoading={isLoading} />
-      <Loading active={isLoading} />
+      <Loading active={isLoading} message={loadingMessage} />
       <ResultSet results={results} />
     </div>
   );
