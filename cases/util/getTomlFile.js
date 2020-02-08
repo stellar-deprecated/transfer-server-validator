f1ed1d612ecd5966469f9fc71a2195338abb8f3d
Mariano Morales <elcuim@gmail.com>
2020-02-07 21:31:17 -0300

    ADD checks for ' / ' in getTomlFile() checks TRANSFER_SERVER and WEB_AUTH_ENDPOINT

--- cases/util/getTomlFile.js
+++ cases/util/getTomlFile.js
@@ -5,6 +5,12 @@ export default async function(domain) {
   let response = await fetch(domain + ".well-known/stellar.toml");
   const text = await response.text();
   const toml = TOML.parse(text);
+  if (toml.WEB_AUTH_ENDPOINT[toml.WEB_AUTH_ENDPOINT.length - 1] !== "/") {
+      toml.WEB_AUTH_ENDPOINT += "/";
+    }    
+  if (toml.TRANSFER_SERVER[toml.TRANSFER_SERVER.length - 1] !== "/") {
+      toml.TRANSFER_SERVER += "/";
+    }
   expect(toml).toBeDefined();
   return toml;
 }
