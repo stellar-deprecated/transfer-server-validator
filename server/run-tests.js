29b7f1030b1a10460d8959fae4d7b4198f7158aa
Michael Feldstein <msfeldstein@gmail.com>
2020-03-16 20:21:24 -0700

    Strip ansi codes using regex

--- server/run-tests.js
+++ server/run-tests.js
@@ -1,4 +1,5 @@
 const runTest = require("./jest-controller/run-test");
+
 module.exports = async (req, res) => {
   // Set up server-sent events
   res.writeHead(200, {
@@ -8,17 +9,6 @@ module.exports = async (req, res) => {
     "Access-Control-Allow-Origin": "*",
   });
 
-  let i = 0;
-  const sendLoadingMessage = (_) => {
-    const message = ["Running Tests", "Still running tests", "Still going"][
-      i++ % 3
-    ];
-    res.write(`data: ${JSON.stringify({ loadingMessage: message })}\n\n`);
-  };
-  sendLoadingMessage();
-
-  const timer = setInterval(sendLoadingMessage, 5000);
   const results = await runTest(req.query.domain, req.query.test);
-  clearInterval(timer);
   res.write(`data: ${JSON.stringify({ results })}\n\n`);
 };
