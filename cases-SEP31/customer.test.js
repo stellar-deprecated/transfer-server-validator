42f55aeed43a6cb16fff3b3b64c2a965a434accc
Jake Urban <jake@stellar.org>
2020-08-07 11:11:28 -0700

    got SEP-12 tests working

--- cases-SEP31/customer.test.js
+++ cases-SEP31/customer.test.js
@@ -1,20 +1,17 @@
 import { loggableFetch } from "../util/loggableFetcher";
 import getTomlFile from "./util/getTomlFile";
-import { getActiveCurrency } from "./util/currency";
 import { getSep10Token } from "../util/sep10";
-import { convertSEP31Fields } from "./util/sep9-fields";
 import { keyPair } from "./util/registeredKeypair";
-import { createCustomer } from "./util/sep12";
+import { createCustomer, getPutRequestBodyObj } from "./util/sep12";
 import { sep12FieldsSchema } from "./util/schema";
 import { randomBytes } from "crypto";
+import FormData from "form-data";
+
+jest.setTimeout(10000);
 
 const urlBuilder = new URL(process.env.DOMAIN);
 const url = urlBuilder.toString();
 const testCurrency = process.env.CURRENCY;
-
-function validateFields(fields) {
-  return;
-}
 
 describe("/customer", () => {
   let toml;
@@ -23,6 +20,7 @@ describe("/customer", () => {
   let customer_id;
   let memo = Buffer.from(randomBytes(32)).toString("base64");
   let memo_type = "hash";
+  let fieldsRequired;
 
   beforeAll(async () => {
     toml = await getTomlFile(url);
@@ -32,13 +30,13 @@ describe("/customer", () => {
     headers = {
       Authorization: `Bearer ${jwt}`,
     };
-    customer_id = await createCustomer(
+    ({ customer_id, fieldsRequired } = await createCustomer(
       keyPair.publicKey(),
       memo,
       memo_type,
       toml.KYC_SERVER,
       jwt,
-    );
+    ));
   });
 
   describe("GET", () => {
@@ -57,11 +55,67 @@ describe("/customer", () => {
       expect(json.status).toEqual("NEEDS_INFO");
       expect(json.fields).toMatchSchema(sep12FieldsSchema);
     });
-    it("returns proper schema for existing customer", () => {});
+
+    it("returns proper schema for existing customer", async () => {
+      let encodedMemo = encodeURIComponent(memo);
+      let { json, logs, status } = await loggableFetch(
+        toml.KYC_SERVER +
+          `/customer?account=${keyPair.publicKey()}&memo=${encodedMemo}&memo_type=${memo_type}`,
+        { headers: headers },
+      );
+      expect(status).toBe(200);
+      expect(["ACCEPTED", "PROCESSING"]).toContain(json.status);
+      expect(json.id).toBe(expect.any(String));
+    });
   });
 
   describe("PUT", () => {
-    it("creates customer for valid request", () => {});
-    it("updates to existing customers are permitted", () => {});
+    it("updates to existing customers are permitted", async () => {
+      // This is actually something not clearly defined in the SEP:
+      // Should anchors allow updates to customers that aren't in
+      // NEEDS_INFO or REJECTED status? I assume so since users
+      // may want to update info from their client.
+      //
+      // This brings up a larger question: should SEP-12 GET /customer
+      // return `fields` for an existing account/memo record (assuming
+      // the client is authenticated)? This may be a security concern
+      // but it would allow clients to show users the info the anchor
+      // has on file for the user.
+      let encodedMemo = encodeURIComponent(memo);
+      let standardVals = await getPutRequestBodyObj(
+        keyPair.publicKey(),
+        memo,
+        memo_type,
+        toml.KYC_SERVER,
+        jwt,
+        fieldsRequired,
+      );
+      if (standardVals["first_name"]) {
+        standardVals["first_name"] += "test";
+      } else {
+        // This test is useless if the anchor doesn't require first names.
+        // I assume every anchor would want this.
+        return;
+      }
+      let formData = new FormData();
+      formData.append("account", standardVals["account"]);
+      formData.append("memo", standardVals["memo"]);
+      formData.append("memo_type", standardVals["memo_type"]);
+      for (let key in standardVals) {
+        if (["account", "memo", "memo_type"].includes(key)) continue;
+        formData.append(key, standardVals[key]);
+      }
+      let { json, logs, status } = await loggableFetch(
+        toml.KYC_SERVER +
+          `/customer?account=${keyPair.publicKey()}&memo=${encodedMemo}&memo_type=${memo_type}`,
+        {
+          headers: headers,
+          method: "PUT",
+          body: formData,
+        },
+      );
+      expect(json.id).toBe(customer_id);
+      expect(status).toBe(202);
+    });
   });
 });
--- cases-SEP31/util/sep12.js
+++ cases-SEP31/util/sep12.js
@@ -2,33 +2,66 @@ import { loggableFetch } from "../../util/loggableFetcher";
 import { values } from "./sep9-fields";
 import FormData from "form-data";
 
-export async function createCustomer(account, memo, memo_type, kycURL, jwt) {
+export async function getPutRequestBodyObj(
+  account,
+  memo,
+  memo_type,
+  kycURL,
+  jwt,
+  fieldsRequired = {},
+) {
   let headers = {
     Authorization: `Bearer ${jwt}`,
   };
-  let encodedMemo = encodeURIComponent(memo);
-  let uri =
-    kycURL +
-    `/customer?account=${account}&memo=${encodedMemo}&memo_type=${memo_type}`;
-  let { json, logs, status } = await loggableFetch(uri, { headers: headers });
-  let fieldsNeeded = Object.keys(json.fields);
-  const formData = new FormData();
-  formData.append("account", account);
-  formData.append("memo", memo);
-  formData.append("memo_type", memo_type);
-  for (let key of fieldsNeeded) {
+  if (Object.keys(fieldsRequired).length == 0) {
+    let encodedMemo = encodeURIComponent(memo);
+    let uri =
+      kycURL +
+      `/customer?account=${account}&memo=${encodedMemo}&memo_type=${memo_type}`;
+    let { json, logs, status } = await loggableFetch(uri, { headers: headers });
+    fieldsRequired = json.fields;
+  }
+  let fieldKeys = Object.keys(fieldsRequired);
+  let customerValues = {
+    account: account,
+    memo: memo,
+    memo_type: memo_type,
+  };
+  for (let key of fieldKeys) {
     if (!values[key]) {
       console.log(`The validator doesn't have a ${key} SEP-9 field`);
       continue;
     }
+    customerValues[key] = values[key];
+  }
+  return { customerValues: customerValues, fieldsRequired: fieldsRequired };
+}
+
+export async function createCustomer(account, memo, memo_type, kycURL, jwt) {
+  let headers = {
+    Authorization: `Bearer ${jwt}`,
+  };
+  let { customerValues, fieldsRequired } = await getPutRequestBodyObj(
+    account,
+    memo,
+    memo_type,
+    kycURL,
+    jwt,
+  );
+  const formData = new FormData();
+  formData.append("account", customerValues["account"]);
+  formData.append("memo", customerValues["memo"]);
+  formData.append("memo_type", customerValues["memo_type"]);
+  for (let key in customerValues) {
+    if (["account", "memo", "memo_type"].includes(key)) continue;
     formData.append(key, values[key]);
   }
-  ({ json, logs, status } = await loggableFetch(kycURL + "/customer", {
+  let { json, logs, status } = await loggableFetch(kycURL + "/customer", {
     headers: headers,
     method: "PUT",
     body: formData,
-  }));
+  });
   expect(status).toEqual(202);
   expect(json.id).toEqual(expect.any(String));
-  return json.id;
+  return { customer_id: json.id, fieldsRequired: fieldsRequired };
 }
