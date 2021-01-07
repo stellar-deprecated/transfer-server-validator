e07be5e0c7d67f44cb26b0b60bbb087d9c294e1a
Lawrence Lawson <lawrence@stellar.org>
2021-01-07 10:36:16 -0800

    create signersAssertion matcher

--- setup.js
+++ setup.js
@@ -1,2 +1,36 @@
 import { matchers } from "jest-json-schema";
 expect.extend(matchers);
+expect.extend({
+  // If anything goes wrong with a test that requires an account that was auto generated
+  // We write the account used in the message along side all the other accounts that were
+  // generated during the current run
+  // This extends the signers support test previous truthy and falsy expectation matchers
+  signersAssertion(token, logs, accountsUsed, accountPool) {
+    const pass = token && logs;
+    let usedAccs = "⬇️ Accounts used ⬇️\n -----------------------------";
+    accountsUsed.forEach((acc) => {
+      usedAccs += `
+          \nPublicKey: ${acc.kp.publicKey()}
+          \nSecretKey: ${acc.kp.secret()}
+          \n -----------------------------`;
+    });
+    let genAccs = "⬇️ Generated Accounts ⬇️\n -----------------------------";
+    accountPool.forEach((acc) => {
+      genAccs += `
+          \nPublicKey: ${acc.kp.publicKey()}
+          \nSecretKey: ${acc.kp.secret()}
+          \n -----------------------------`;
+    });
+    if (pass) {
+      return {
+        message: () => ``,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `Error handeling ${usedAccs}\n${genAccs}`,
+        pass: false,
+      };
+    }
+  },
+});
